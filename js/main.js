8768910046ae2922b0ca9dd7eb96a5feb79a837a
a-emanuel <79093616+a-emanuel@users.noreply.github.com>
2021-05-02 01:31:21 +0300

    nav-bar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
+const toggleButton = document.getElementById("nav-menu-btn");
+const toggleButton2 = document.getElementById("nav-menu-btn2");
 const collapsibles = document.querySelectorAll(".collapsible");
+
 collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
+  toggleButton.addEventListener("click", function () {
+    item.classList.toggle("collapsible--expanded");
+  })
+);
+
+collapsibles.forEach((item) =>
+  toggleButton2.addEventListener("click", function () {
+    item.classList.toggle("nav--expanded");
   })
 );
